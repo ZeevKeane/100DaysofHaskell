4d3a646acdca7c2b3cab4f271fe07e48cf87a132
ZeevKeane <63415815+ZeevKeane@users.noreply.github.com>
2022-05-21 11:28:49 -0400

    Create Day 1 - Introduction to Haskell.hs
    https://hyp.is/go?url=http%3A%2F%2Flearnyouahaskell.com%2Fintroduction&group=y4eeb5Mr

---
+++ Day 1 - Introduction to Haskell.hs
@@ -0,0 +1,7 @@
+doubleMe x = x + x  
+
+-- I studied an introduction to the language, what is haskell, why use haskell, what are some of the differences between impairative and functional programming languages. 
+-- I also learned about refrential transparency vs opacity 
+-- The lazyness of Haskell
+
+-- For all my notes directly taken from http://learnyouahaskell.com/introduction: https://hyp.is/go?url=http%3A%2F%2Flearnyouahaskell.com%2Fintroduction&group=y4eeb5Mr
